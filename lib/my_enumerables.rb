ea92fae039af26812d942e36a250c97cfa6e384f
teredos <61703746+teredos@users.noreply.github.com>
2023-02-23 15:32:51 +0000

    add reimplemented enumerable methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,71 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    length.times { |i| yield(self[i], i) }
+    self
+  end
+
+  def my_select
+    new_arr = []
+    length.times { |i| new_arr << self[i] if yield(self[i]) }
+    new_arr
+  end
+
+  def my_all?
+    true_arr = []
+    length.times { |i| true_arr << self[i] if yield(self[i]) }
+    true_arr.length == length
+  end
+
+  def my_any?
+    true_arr = []
+    length.times { |i| true_arr << self[i] if yield(self[i]) }
+    true_arr.length.positive?
+  end
+
+  def my_none?
+    true_arr = []
+    length.times { |i| true_arr << self[i] if yield(self[i]) }
+    true_arr.empty?
+  end
+
+  def my_count
+    return length unless block_given?
+
+    new_arr = []
+    length.times { |i| new_arr << self[i] if yield(self[i]) }
+    new_arr.length
+  end
+
+  def my_map
+    new_arr = []
+    length.times { |i| new_arr << yield(self[i]) }
+    new_arr
+  end
+
+  def my_inject(*args)
+    if args[0].is_a?(Symbol)
+      sum = self[0]
+      (length - 1).times { |i| sum = sum.send(args[0], self[i + 1]) }
+      sum
+    elsif args[1].is_a?(Symbol)
+      sum = args[0]
+      length.times { |i| sum = sum.send(args[1], self[i]) }
+      sum
+    elsif args[0].is_a?(Integer)
+      sum = args[0]
+      length.times { |i| sum = yield(sum, self[i]) }
+      sum
+    elsif args.empty?
+      sum = self[0]
+      (length - 1).times { |i| sum = yield(sum, self[i + 1]) }
+      sum
+    end
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    length.times { |i| yield(self[i]) }
+    self
+  end
 end
